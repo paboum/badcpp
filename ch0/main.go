a564228f6666c6be4258644601991bf811e11ae0
paboum <pabou@protonmail.com>
2020-09-26 17:13:52 +0200

    Proving encapsulated.Trace is possible to declare or copy just like any object

--- ch0/main.go
+++ ch0/main.go
@@ -14,10 +14,19 @@ func main() {
 	t1.Off()
 	t1.Print("Y1")
 
-	//t2 := encapsulated.TraceCustom(os.Stderr)
-	t2 := encapsulated.TraceDefault()
+	t2 := encapsulated.TraceCustom(os.Stderr)
 	t2.On()
 	t2.Print("X2")
 	t2.Off()
 	t2.Print("Y2")
+
+	var t3 encapsulated.Trace
+	t3 = encapsulated.TraceDefault()
+	t3.On()
+	t3.Print("X3")
+	t3.Off()
+	t3.Print("Y3")
+	t3 = t2
+	t3.On()
+	t3.Print("Z3")
 }
